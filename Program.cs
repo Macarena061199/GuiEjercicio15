8902cc45fc4697a0c171e33dbbb0bcd7303e2cb2
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-03 21:34:47 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+  
+  /* Solicite un ingreso al usuario y determine si puede ser interpretado como un valor numérico.*/
+   bool flag = false;
+do
+{
+    Console.Write("Por favor ingrese un número: ");
+    string ingreso = Console.ReadLine();
+
+    if (!int.TryParse(ingreso, out int salida))
+    {
+        Console.WriteLine("El valor ingresado debe ser numérico.");
+    }
+    else
+    {
+        flag = true;
+    }
+
+    
+} while (flag == false);
+
+
+
+
+
+
+
+
+
